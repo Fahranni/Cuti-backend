6516474b7fe3dbe690d27d2af1425ccb808a7245
Alledanaralle <chrisraditya2005@gmail.com>
2025-05-17 12:35:10 +0700

    major fixes actually

--- app/Controllers/Kajur.php
+++ app/Controllers/Kajur.php
@@ -7,15 +7,17 @@ use App\Models\UserModel; // Tambahkan model untuk tabel user
 use App\Models\DosenModel; // Tambahkan model untuk tabel admin
 use CodeIgniter\API\ResponseTrait;
 
-header('Access-Control-Allow-Origin: *'); // Atau ganti * dengan URL Laravel Anda
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, X-Requested-With, Authorization');
+header("Access-Control-Allow-Origin: *"); // Atau ganti * dengan URL Laravel Anda
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header(
+    "Access-Control-Allow-Headers: Content-Type, X-Requested-With, Authorization"
+);
 
 class Kajur extends BaseController
 {
     use ResponseTrait;
     protected $kajurModel; // Ganti jadi protected agar lebih terorganisir
-    protected $userModel;  // Tambahkan untuk akses UserModel
+    protected $userModel; // Tambahkan untuk akses UserModel
     protected $dosenModel;
 
     public function __construct()
@@ -46,38 +48,43 @@ class Kajur extends BaseController
         $data = $this->request->getPost();
 
         // Validasi: cek apakah id_user dan username sesuai di tabel user
-        $userCheck = $this->userModel->where('id_user', $data['id_user'])
-            ->where('username', $data['nama_kajur'])
-            ->first();
-        $nidnCheck = $this->dosenModel->where('nidn', $data['nidn'])
-            ->where('nama_dosen', $data['nama_kajur'])
+        $userCheck = $this->userModel
+            ->where("id_user", $data["id_user"])
+            ->where("username", $data["nama_kajur"])
             ->first();
 
-        if (!$userCheck || !$nidnCheck) {
-            return $this->fail([
-                'message' => 'ID User, username, atau NIDN tidak sesuai dengan data di tabel user atau dosen'
-            ], 400);
+        if (!$userCheck) {
+            return $this->fail(
+                [
+                    "message" =>
+                        "ID User atau username tidak sesuai dengan data di tabel user",
+                ],
+                400
+            );
         }
 
-        $kajurCheck = $this->kajurModel->where('nama_kajur', $data['nama_kajur'])->first();
+        $kajurCheck = $this->kajurModel
+            ->where("nama_kajur", $data["nama_kajur"])
+            ->first();
         if ($kajurCheck) {
-            return $this->fail([
-                'message' => 'Nama Kajur sudah ada di tabel kajur'
-            ], 400);
+            return $this->fail(
+                [
+                    "message" => "Nama Kajur sudah ada di tabel kajur",
+                ],
+                400
+            );
         }
 
-        $jurusanCheck = $this->kajurModel->where('nama_jurusan', $data['nama_jurusan'])->first();
+        $jurusanCheck = $this->kajurModel
+            ->where("nama_jurusan", $data["nama_jurusan"])
+            ->first();
         if ($jurusanCheck) {
-            return $this->fail([
-                'message' => 'Nama Jurusan sudah ada di tabel kajur'
-            ], 400);
-        }
-
-        $dosenNameCheck = $this->dosenModel->where('nama_dosen', $data['nama_kajur'])->first();
-        if (!$dosenNameCheck) {
-            return $this->fail([
-                'message' => 'Nama Kajur tidak ditemukan di tabel dosen'
-            ], 400);
+            return $this->fail(
+                [
+                    "message" => "Nama Jurusan sudah ada di tabel kajur",
+                ],
+                400
+            );
         }
 
         // Simpan data ke tabel admin
@@ -86,11 +93,11 @@ class Kajur extends BaseController
         }
 
         $response = [
-            'status' => 200,
-            'error' => null,
-            'message' => [
-                'success' => 'Berhasil Menambah Data',
-            ]
+            "status" => 200,
+            "error" => null,
+            "message" => [
+                "success" => "Berhasil Menambah Data",
+            ],
         ];
         return $this->respond($response, 200);
     }
@@ -98,56 +105,55 @@ class Kajur extends BaseController
     public function update($id = null)
     {
         $data = $this->request->getRawInput();
-        $data['id_kajur'] = $id;
+        $data["id_kajur"] = $id;
 
         // Check if record exists in admin table
-        $ifExist = $this->kajurModel->where('id_kajur', $id)->findAll();
+        $ifExist = $this->kajurModel->where("id_kajur", $id)->findAll();
         if (!$ifExist) {
             return $this->failNotFound("Data tidak ditemukan");
         }
 
         // Validasi: cek apakah id_user dan username sesuai di tabel user
-        $userCheck = $this->userModel->where('id_user', $data['id_user'])
-            ->where('username', $data['nama_kajur'])
+        $userCheck = $this->userModel
+            ->where("id_user", $data["id_user"])
+            ->where("username", $data["nama_kajur"])
             ->first();
 
-        $nidnCheck = $this->dosenModel->where('nidn', $data['nidn'])
-            ->where('nama_dosen', $data['nama_kajur'])
-            ->first();
-
-        if (!$userCheck || !$nidnCheck) {
-            return $this->fail([
-                'message' => 'ID User, username, atau NIDN tidak sesuai dengan data di tabel user atau dosen'
-            ], 400);
+        if (!$userCheck) {
+            return $this->fail(
+                [
+                    "message" =>
+                        "ID User atau username tidak sesuai dengan data di tabel user",
+                ],
+                400
+            );
         }
 
-        $kajurCheck = $this->kajurModel->where('nama_kajur', $data['nama_kajur'])
-            ->where('id_kajur !=', $id)
+        $kajurCheck = $this->kajurModel
+            ->where("nama_kajur", $data["nama_kajur"])
+            ->where("id_kajur !=", $id)
             ->first();
         if ($kajurCheck) {
-            return $this->fail([
-                'message' => 'Nama Kajur sudah ada di tabel kajur'
-            ], 400);
+            return $this->fail(
+                [
+                    "message" => "Nama Kajur sudah ada di tabel kajur",
+                ],
+                400
+            );
         }
 
-
-        $jurusanCheck = $this->kajurModel->where('nama_jurusan', $data['nama_jurusan'])
-            ->where('id_kajur !=', $id)
+        $jurusanCheck = $this->kajurModel
+            ->where("nama_jurusan", $data["nama_jurusan"])
+            ->where("id_kajur !=", $id)
             ->first();
         if ($jurusanCheck) {
-            return $this->fail([
-                'message' => 'Nama Jurusan sudah ada di tabel kajur'
-            ], 400);
+            return $this->fail(
+                [
+                    "message" => "Nama Jurusan sudah ada di tabel kajur",
+                ],
+                400
+            );
         }
-
-
-        $dosenNameCheck = $this->dosenModel->where('nama_dosen', $data['nama_kajur'])->first();
-        if (!$dosenNameCheck) {
-            return $this->fail([
-                'message' => 'Nama Kajur tidak ditemukan di tabel dosen'
-            ], 400);
-        }
-
 
         // Simpan perubahan ke tabel admin
         if (!$this->kajurModel->save($data)) {
@@ -155,26 +161,26 @@ class Kajur extends BaseController
         }
 
         $response = [
-            'status' => 200,
-            'error' => null,
-            'message' => [
-                'success' => 'Berhasil Mengubah Data',
-            ]
+            "status" => 200,
+            "error" => null,
+            "message" => [
+                "success" => "Berhasil Mengubah Data",
+            ],
         ];
         return $this->respond($response, 200);
     }
 
     public function delete($id = null)
     {
-        $data = $this->kajurModel->where('id_kajur', $id)->findAll();
+        $data = $this->kajurModel->where("id_kajur", $id)->findAll();
         if ($data) {
             $this->kajurModel->delete($id);
             $response = [
-                'status' => 200,
-                'error' => null,
-                'message' => [
-                    'success' => 'Berhasil Menghapus Data',
-                ]
+                "status" => 200,
+                "error" => null,
+                "message" => [
+                    "success" => "Berhasil Menghapus Data",
+                ],
             ];
             return $this->respondDeleted($response);
         } else {
